324840057daf5839719e752a0db4ba65e454e46d
kcjfgnl9205 <kcjfgnl9205@gmail.com>
2025-09-28 21:05:04 +0900

    fix: 텍스트 삭제

--- src/utils/objects/answer.ts
+++ src/utils/objects/answer.ts
@@ -12,6 +12,23 @@ export class AnswerObject extends CommonObject {
     const { coordinatesArr } = props
     this.type = 'answer'
     this.coordinatesArr = coordinatesArr
+    this.coordinates = this.initCoordinates()
+  }
+
+  private initCoordinates() {
+    const arr = this.coordinatesArr.flat()
+
+    const precision = 5 // 소수점 3자리까지만 반올림해서 중복 판정
+    const seen = new Set()
+    const uniqueArr = []
+    for (const [x, y] of arr) {
+      const key = `${x.toFixed(precision)},${y.toFixed(precision)}` // 판정용 키
+      if (!seen.has(key)) {
+        seen.add(key)
+        uniqueArr.push([x, y]) // 원래 값 그대로 보관
+      }
+    }
+    return uniqueArr
   }
 }
 

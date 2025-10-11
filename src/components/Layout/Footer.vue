783ce729f86a286464b7ee09744056d108be7363
kcjfgnl9205 <kcjfgnl9205@gmail.com>
2025-10-11 13:25:39 +0900

    fix: 메일제거

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -2,6 +2,7 @@
 
 <template>
   <footer class="translate-y-0 w-full py-4 bg-white text-center">
-    © 2025 Tangram · Play · Learn · Enjoy · Contact: kcjfgnl9205@gmail.com
+    © 2025 Tangram · Play · Learn · Enjoy
   </footer>
+  <!-- //· Contact: kcjfgnl9205@gmail.com -->
 </template>
